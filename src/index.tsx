d8afcec1fe9253df197f9129e745ab3d356dd12c
SPH-0gJo <yjjo@sphinfo.co.kr>
2023-08-17 16:09:34 +0900

    Design. 퍼블리싱 반영

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 //import "./index.css";
 import "@/resources/scss/main.scss";
-// import App from "./App";
+import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./views/Login/Login";
 import Default from "./views/Default";
@@ -16,7 +16,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/">
+      <Route path="/" element={<App />}>
         <Route index element={<Default />} />
         <Route path="dashboard" element={<DashBoard />}>
           <Route path="one" />
