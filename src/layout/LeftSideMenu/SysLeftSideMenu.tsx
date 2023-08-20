c28e8681828e0abdffe45295f97c019e6b0f3173
SPH-0gJo <yjjo@sphinfo.co.kr>
2023-08-20 13:43:31 +0900

    Design. 메뉴 Active 관련 임시 DOM 조작 JS 코드 추가

--- src/layout/LeftSideMenu/SysLeftSideMenu.tsx
+++ src/layout/LeftSideMenu/SysLeftSideMenu.tsx
@@ -7,31 +7,31 @@ const SysLeftSideMenu = function () {
         <div className="side-menu-title">시스템 관리</div>
         <ul className="side-menu">
           <li className="side-nav-item">
-            <a href="" className="side-nav-link">
+            <a href="#" className="side-nav-link">
               <i className="mdi mdi-account-group-outline" />
               <span>그룹 관리</span>
             </a>
           </li>
           <li className="side-nav-item menuitem-active">
-            <a href="" className="side-nav-link">
+            <a href="#" className="side-nav-link">
               <i className="mdi mdi-card-account-details-outline" />
               <span>사용자 관리</span>
             </a>
           </li>
           <li className="side-nav-item">
-            <a href="" className="side-nav-link">
+            <a href="#" className="side-nav-link">
               <i className="mdi mdi-folder-wrench-outline" />
               <span>메뉴 관리</span>
             </a>
           </li>
           <li className="side-nav-item">
-            <a href="" className="side-nav-link">
+            <a href="#" className="side-nav-link">
               <i className="mdi mdi-lock-outline" />
               <span>접근 권한 관리</span>
             </a>
           </li>
           <li className="side-nav-item">
-            <a href="" className="side-nav-link">
+            <a href="#" className="side-nav-link">
               <i className="mdi mdi-history" />
               <span>접속 이력</span>
             </a>
--- src/layout/LeftSideMenu/index.tsx
+++ src/layout/LeftSideMenu/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import DashLeftSideMenu from "./DashLeftSideMenu";
 import { useLocation } from "react-router-dom";
 import SysLeftSideMenu from "./SysLeftSideMenu";
@@ -6,6 +6,29 @@ import SysLeftSideMenu from "./SysLeftSideMenu";
 const LeftSideMenu = function () {
   const { pathname } = useLocation();
   console.log("----pathname----", pathname);
+
+  useEffect(() => {
+    var sideNavLinks = document.querySelectorAll(".side-nav-link");
+
+    sideNavLinks.forEach(function (link) {
+      link.addEventListener("click", (e) => {
+        var label = link as HTMLAnchorElement;
+        var parent = label.closest(".has-sub");
+        var list = label.nextElementSibling as HTMLUListElement | null;
+
+        if (!list || !parent) return;
+
+        if (parent.classList.contains("is-open")) {
+          list.style.display = "none";
+          parent.classList.remove("is-open");
+        } else {
+          list.style.display = "block";
+          parent.classList.add("is-open");
+        }
+      });
+    });
+  }, []);
+
   return pathname.startsWith("/system") ? (
     <SysLeftSideMenu />
   ) : (
