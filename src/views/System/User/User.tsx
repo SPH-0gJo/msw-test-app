ab991b2aefbda7b4a94b390fc3736751d9100576
SPH-0gJo <yjjo@sphinfo.co.kr>
2023-09-07 15:13:22 +0900

    feat : #4 사용자 등록 모달 별도 컴포넌트로 생성

--- src/views/System/User/User.tsx
+++ src/views/System/User/User.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
-import { Button, Modal } from "react-bootstrap";
+import UserRegisterModal from "./UserRegisterModal";
 
 const User = function () {
-  const [standard, setStandard] = useState<boolean>(false);
+  //등록 모달
+  const [regModalShow, setRegModalShow] = useState<boolean>(false);
 
   // Show/hide the modal
-  const toggleStandard = () => {
-    setStandard(!standard);
+  const toggleRegModal = () => {
+    setRegModalShow(!regModalShow);
   };
 
   return (
@@ -27,7 +28,7 @@ const User = function () {
             </div>
             <div className="btn-wrap">
               <button
-                onClick={toggleStandard}
+                onClick={toggleRegModal}
                 className="btn btn-sm rounded-pill btn-primary"
               >
                 <i className="fe-edit" />
@@ -321,93 +322,7 @@ const User = function () {
         </div>
       </div>
       {/* 모달창 */}
-      <Modal show={standard} onHide={toggleStandard} className="custom-modal">
-        <Modal.Header onHide={toggleStandard} closeButton>
-          <h4 className="modal-title">등록</h4>
-        </Modal.Header>
-        <Modal.Body>
-          <div className="form-wrap">
-            <form>
-              <div className="mb-2">
-                <label className="form-label">그룹</label>
-                <select
-                  aria-label="Default select example"
-                  className="form-select"
-                >
-                  <option>시의원</option>
-                  <option value="1">관리자</option>
-                  <option value="2">사용자</option>
-                </select>
-              </div>
-              <div className="mb-2">
-                <label className="form-label">이름</label>
-                <input
-                  placeholder="이름을 입력해주세요."
-                  name="username"
-                  type="text"
-                  id="username"
-                  className="form-control"
-                />
-              </div>
-              <div className="mb-2">
-                <label className="form-label">아이디</label>
-                <input
-                  placeholder="아이디를 입력해주세요."
-                  name="email"
-                  type="email"
-                  id="email"
-                  className="form-control"
-                />
-              </div>
-
-              <div className="mb-2">
-                <label className="form-label">비밀번호</label>
-                <input
-                  placeholder="비밀번호를 입력해주세요."
-                  name="password"
-                  type="password"
-                  id="password"
-                  className="form-control"
-                />
-              </div>
-              <div className="mb-2">
-                <label className="form-label">비밀번호 확인</label>
-                <input
-                  placeholder="비밀번호를 확인해주세요."
-                  name="confirmpassword"
-                  type="password"
-                  id="confirmpassword"
-                  className="form-control"
-                />
-              </div>
-              <div className="mb-2">
-                <label className="form-label">관리자</label>
-                <select
-                  aria-label="Default select example"
-                  className="form-select"
-                >
-                  <option value="1">관리자</option>
-                  <option value="2">사용자</option>
-                </select>
-              </div>
-            </form>
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <div className="btn-wrap">
-            <Button
-              variant="secondary"
-              onClick={toggleStandard}
-              className="btn-sm rounded-pill"
-            >
-              <i className="fe-x-circle"></i>취소
-            </Button>
-            <Button onClick={toggleStandard} className="btn-sm rounded-pill">
-              <i className="fe-edit"></i>등록
-            </Button>
-          </div>
-        </Modal.Footer>
-      </Modal>
+      <UserRegisterModal show={regModalShow} toggleShow={toggleRegModal} />
     </>
   );
 };
