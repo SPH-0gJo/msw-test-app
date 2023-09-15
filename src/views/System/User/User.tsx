d05b4ebd50ff83f0f58f61e20a8f1a6b3e92308a
SPH-0gJo <yjjo@sphinfo.co.kr>
2023-09-15 16:14:59 +0900

    Feat : #18 페이지 컴포넌트들  클릭시 테이블 데이터 변경 로직 추가

--- src/views/System/User/User.tsx
+++ src/views/System/User/User.tsx
@@ -1,13 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import UserRegisterModal from "@/component/User/UserRegisterModal";
 import Table from "@/component/ui-components/Table";
 import CheckBox from "@/component/ui-components/CheckBox";
 import { UserData, getUserTableData, users } from "@/shared/var/user";
 import Pagination from "@/component/ui-components/Pagination";
+import PageNext from "@/component/ui-components/PageNext";
+import PagePrev from "@/component/ui-components/PagePrev";
+import PageEllipsis from "@/component/ui-components/PageEllipsis";
+import { usePagination } from "@/shared/pagination";
+import PageItem from "@/component/ui-components/PageItem";
 
 export type Column = {
   key: string;
   value: string | JSX.Element;
+};
+
+const filterData = function (pageSize: number, data: any[], page: number) {
+  const startIdx = (page - 1) * pageSize;
+  return data.slice(startIdx, startIdx + pageSize);
 };
 
 const User = function () {
@@ -19,7 +29,30 @@ const User = function () {
     setRegModalShow(!regModalShow);
   };
 
-  const data = getUserTableData(users);
+  const pageSize = 10,
+    initPage = 1;
+
+  //pageSize는 고정이므로 매번 넣기보다는 Currying...
+  const pageSizedFilterData = filterData.bind(null, pageSize);
+
+  const rawData = getUserTableData(users);
+  const initData = pageSizedFilterData(rawData, initPage);
+
+  const [data, setData] = useState(initData);
+
+  const {
+    pageList,
+    page,
+    hasPrev,
+    hasNext,
+    lastPage,
+    hasGoLast,
+    hasGoFirst,
+    firstPage,
+    setPage,
+  } = usePagination(rawData, initPage, pageSize);
+
+  useEffect(() => setData(pageSizedFilterData(rawData, page)), [page]);
 
   //Column의 key는 data의 정보를 가져 오기위해서는 data 객체의 key와 동일해야함.
 
@@ -87,7 +120,61 @@ const User = function () {
           <div className="table-wrap">
             <Table<UserData> columns={columns} data={data} />
           </div>
-          <Pagination data={data} />
+          <Pagination>
+            <PagePrev
+              onClick={() => {
+                if (!hasPrev) return;
+                setPage((prevState) => prevState - 1);
+              }}
+              disabled={!hasPrev}
+            />
+
+            {hasGoFirst && (
+              <>
+                <PageItem
+                  onClick={() => {
+                    setPage(firstPage);
+                  }}
+                >
+                  {firstPage}
+                </PageItem>
+                <PageEllipsis />
+              </>
+            )}
+
+            {pageList.map((pg) => (
+              <PageItem
+                onClick={() => {
+                  setPage(pg);
+                }}
+                key={pg}
+                active={pg === page}
+              >
+                {pg}
+              </PageItem>
+            ))}
+
+            {hasGoLast && (
+              <>
+                <PageEllipsis />
+                <PageItem
+                  onClick={() => {
+                    setPage(lastPage);
+                  }}
+                >
+                  {lastPage}
+                </PageItem>
+              </>
+            )}
+
+            <PageNext
+              onClick={() => {
+                if (!hasNext) return;
+                setPage((prevState) => prevState + 1);
+              }}
+              disabled={!hasNext}
+            />
+          </Pagination>
         </div>
       </div>
       {/* 모달창 */}
