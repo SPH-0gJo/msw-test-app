95a5bfd6614519bbadc8faa21612593c79f34107
SPH-0gJo <yjjo@sphinfo.co.kr>
2023-09-26 10:25:08 +0900

    Feat : form 구성중 ....

--- src/component/User/UserModifyModal.tsx
+++ src/component/User/UserModifyModal.tsx
@@ -20,12 +20,32 @@ const UserModifyModal = function ({
 
   console.log("UserModifyModal", user);
 
+  /**
+   * 등록 모달의 input, select 태그등은 그대로 쓰되
+   * 공통으로 빼낼수 있는 함수들은 빼내기 (validationCallback 등)
+   * + groups는 store에 저장해서 한번만 불러오기
+   */
+
   return (
     <Modal show={show} onHide={formHideHandler} className="custom-modal">
       <Modal.Header onHide={formHideHandler} closeButton>
         <h4 className="modal-title">수정</h4>
       </Modal.Header>
-      <Modal.Body></Modal.Body>
+      <Modal.Body>
+        <div className="form-wrap">
+          <form>
+            <div className="mb-2">
+              <label className="form-label">그룹</label>
+              <select
+                aria-label="Default select example"
+                className="form-select"
+              >
+                <option value="">선택 없음 (게스트로 등록)</option>
+              </select>
+            </div>
+          </form>
+        </div>
+      </Modal.Body>
       <Modal.Footer>
         <div className="btn-wrap">
           <Button
