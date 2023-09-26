fa67af69cf57d283877493df0eaaf7e306030aab
SPH-0gJo <yjjo@sphinfo.co.kr>
2023-09-26 11:09:40 +0900

    Feat : Modal  form 작성중,,

--- src/component/User/UserModifyModal.tsx
+++ src/component/User/UserModifyModal.tsx
@@ -34,6 +34,7 @@ const UserModifyModal = function ({
       <Modal.Body>
         <div className="form-wrap">
           <form>
+            {/* 그룹 */}
             <div className="mb-2">
               <label className="form-label">그룹</label>
               <select
@@ -42,6 +43,45 @@ const UserModifyModal = function ({
               >
                 <option value="">선택 없음 (게스트로 등록)</option>
               </select>
+            </div>
+            {/* 이름 */}
+            <div className="mb-2">
+              <label className="form-label">이름</label>
+              <input
+                placeholder="이름을 입력해주세요."
+                type="text"
+                id="userName"
+                className="form-control"
+              />
+            </div>
+            {/* 아이디 */}
+            <div className="mb-2">
+              <label className="form-label">아이디</label>
+              <input
+                placeholder="아이디를 입력해주세요."
+                type="text"
+                id="userId"
+                className="form-control"
+              />
+            </div>
+            {/* 비밀번호 */}
+            <div className="mb-2">
+              <label className="form-label">비밀번호</label>
+              <input
+                placeholder="비밀번호를 입력해주세요."
+                type="password"
+                id="password"
+                className="form-control"
+              />
+            </div>
+            {/* 비밀번호 확인 */}
+            <div className="mb-2">
+              <label className="form-label">비밀번호 확인</label>
+              <input
+                placeholder="비밀번호를 확인해주세요."
+                type="password"
+                className="form-control"
+              />
             </div>
           </form>
         </div>
