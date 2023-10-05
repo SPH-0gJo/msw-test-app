5c7b6978932110c3fa79cdfc5f233ac785ff1179
SPH-0gJo <yjjo@sphinfo.co.kr>
2023-10-05 10:26:21 +0900

    Feat : #37 수정모달창 생성 및 세팅

---
+++ src/component/Group/GroupModifyModal.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { FormModalProps } from "@/shared/type/modal";
+import CustomFormModal from "../CustomFormModal";
+import GroupForm, { GroupFormInputs } from "./GroupForm";
+import { SubmitErrorHandler, SubmitHandler } from "react-hook-form";
+import { useStores } from "@/modules/Store";
+import { ERROR, SUCCESS } from "@/shared/var/msg";
+import { Group } from "@/shared/var/group";
+
+interface GroupModifyModalProps extends FormModalProps {
+  group: Group | null;
+}
+
+const GroupModifyModal = function (props: GroupModifyModalProps) {
+  const formId = "group-form-mod";
+
+  const { toggleShow, onSubmitSuccess } = props;
+
+  const formHideHandler = () => {
+    toggleShow();
+  };
+
+  const { groupStore } = useStores();
+
+  const handleFormValid: SubmitHandler<GroupFormInputs> = async function (
+    data
+  ) {
+    console.log("모든 필드 validation 후 문제 없을 때 호출");
+
+    try {
+      await groupStore.addGroup(data.groupName);
+      alert(SUCCESS.PROCCESSED);
+      //팝업 창 리셋 후 닫기
+      formHideHandler();
+      //데이터 불러오기
+      onSubmitSuccess();
+    } catch (error) {
+      console.error(error);
+      alert(ERROR.NOT_PROCESSED);
+    }
+  };
+
+  const handleFormInvalid: SubmitErrorHandler<GroupFormInputs> = function () {
+    console.log("필드 중 유효하지 않은 값이 있을 때 호출");
+  };
+
+  return (
+    <CustomFormModal
+      {...props}
+      title="수정"
+      formId={formId}
+      formHideHandler={formHideHandler}
+    >
+      <GroupForm
+        formId={formId}
+        onFormInvalid={handleFormInvalid}
+        onFormValid={handleFormValid}
+      />
+    </CustomFormModal>
+  );
+};
+
+export default GroupModifyModal;
