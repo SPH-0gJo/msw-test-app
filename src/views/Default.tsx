ea78b567f0fc7baee5f638636fc30f289a1bc850
SPH-0gJo <yjjo@sphinfo.co.kr>
2023-10-17 15:33:43 +0900

    etc : #49 디폴트 경로 변경

--- src/views/Default.tsx
+++ src/views/Default.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const Default = function () {
-  return <Navigate to="/dashboard" />;
+  return <Navigate to="/" />;
 };
 
 export default Default;
