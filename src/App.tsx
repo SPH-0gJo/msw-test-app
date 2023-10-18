acb9008414598853488ef86b59c543966e2f6aca
SPH-0gJo <yjjo@sphinfo.co.kr>
2023-10-18 15:31:03 +0900

    Feat : #49 localStorage가 아닌 매번 새로고침(앱 전체 렌더링) 될 때마다 새로 가져와서 state로 저장하도록 코드 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import "@/resources/scss/main.scss";
 
 import { StoreProvider, stores } from "@/modules/Store";
-import { authMenuInfoList } from "./shared/var/authMenu";
-import AppRoutes from "./AppRoutes";
 
-console.log("authMenuInfoList", authMenuInfoList);
+import AppRoutes from "./AppRoutes";
 
 const App = function () {
   return (
--- src/modules/Login/AuthStore.ts
+++ src/modules/Login/AuthStore.ts
@@ -12,14 +12,25 @@ class AuthStore {
   @observable
   isLoggedIn = Boolean(localStorage.getItem(TOKEN));
   @observable
-  authMenuInfoList: MenuInfo[] = JSON.parse(
-    localStorage.getItem("authMenu") || "[]"
-  );
+  authMenuInfoList: MenuInfo[] = [];
+  // authMenuInfoList: MenuInfo[] = JSON.parse(
+  //   localStorage.getItem("authMenu") || "[]"
+  // );
   rootStore: RootStore;
 
   constructor(rootStore: RootStore) {
     this.rootStore = rootStore;
+    console.log("새로고침");
+
     makeObservable(this);
+
+    //결과가 이전과 동일하다면 (이건 서버에서도 Cache-Control:max-age=0 설정을 해주어야함.. )
+    //https://toss.tech/article/smart-web-service-cache
+    // this.authMenuInfoList = JSON.parse(localStorage.getItem("authMenu"));
+    //아니라면 getAuthMenuList후 새롭게 this.authMenuInfoLis ...
+    if (this.isLoggedIn) {
+      this.configAuthMenuInfoList();
+    }
   }
 
   async login(userId: string, password: string) {
@@ -40,7 +51,7 @@ class AuthStore {
     const result = await this.getAuthMenuList();
     const authMenuInfoList = getMenuInfoList(result.data);
     this.authMenuInfoList = authMenuInfoList;
-    localStorage.setItem("authMenu", JSON.stringify(authMenuInfoList));
+    // localStorage.setItem("authMenu", JSON.stringify(authMenuInfoList));
   }
 
   logout() {
